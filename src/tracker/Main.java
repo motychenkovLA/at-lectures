10e6b7ce28b13546ff4e0d8bd727eeea348c6737
vborodkina <vborodkina1510@gmail.com>
2021-02-08 13:02:29 +0300

    домашняя работа 2

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,8 +1,25 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Просьба заполнить резюме дефекта");
+        String summary = in.nextLine();
+        System.out.println("Просьба выбрать критичность дефекта");
+        System.out.println("- Блокирующий");
+        System.out.println("- Критический");
+        System.out.println("- Средний");
+        System.out.println("- Низкий");
+        String priority = in.next();
+        System.out.println("Просьба ввести ожидаемое количество дней на исправление дефекта");
+        int days = in.nextInt();
+        int week = 5;
+        boolean correction = days > week;
+        System.out.print("Резюме: "+ summary + "|Серьезность: "+ priority + "|Количество дней на исправление: "+ days + "|Займет больше рабочей недели: "+ correction );
 
     }
 
