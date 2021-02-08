cc9e8aa89caf70d0da6f7f4d1ecc6c8bcec8f2ec
siroezhka27 <siroezhka13@gmail.com>
2021-02-08 08:43:16 +0300

    Задание "2"

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,8 +1,28 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        final int CONST = 5;
+        boolean t;
+
+        Scanner ra = new Scanner(System.in);
+        System.out.println("Введите резюме дефекта:");
+        String r = ra.nextLine();
+
+        Scanner ca = new Scanner(System.in);
+        System.out.println("Введите критичность дефекта (блокирующая, критическая, средняя, низкая):");
+        String c = ca.nextLine();
+
+        Scanner da = new Scanner(System.in);
+        System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
+        int d = da.nextInt();
+        t = (d > CONST);
+
+            System.out.println("резюме " +r+ " | серьезность " +c+ " | количество дней на исправление " +d+ " | займет больше рабочей недели " +t);
+
 
     }
 
