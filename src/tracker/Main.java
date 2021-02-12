da36a91d2b05ad5710969ecbdb56f8f1e9356a3f
vsafronov <vsafronov@tsconsulting.com>
2021-02-12 14:13:42 +0300

    homework 3. Добавлены массивы для полей списка дефектов

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,6 +1,5 @@
 package tracker;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,13 +13,20 @@ public class Main {
         // массив возможных критичностей дефекта
         final String[]  CRITICALITY_ARRAY= {"Блокирующий", "Высокий", "Средний", "Низкий"};
 
-        // массив дефектов
-        String[] arrayOfBugs = new String[10];
-        int bug = 0;
-
-        Scanner in = new Scanner(System.in);
         //переменная выбора действия
         int action;
+        // массив дефектов
+        String[] arrayOfBugs = new String[MAX_AMOUNT_BUGS];
+        //переменная текущего дефекта
+        int bug = 0;
+        // массив инфо по резюме
+        String[] resumeArrayInfo = new String[MAX_AMOUNT_BUGS];
+        // массив инфо по критичностям дефектов
+        String[] criticalityArrayInfo = new String[MAX_AMOUNT_BUGS];
+        // массив инфо по дням на исправление
+        int[] daysToFixArrayInfo = new int[MAX_AMOUNT_BUGS];
+
+        Scanner in = new Scanner(System.in);
 
         do {
 
@@ -33,28 +39,29 @@ public class Main {
 
             if (action == 1 && bug < MAX_AMOUNT_BUGS){
                 System.out.println("Введите резюме дефекта:");
-                String resume = in.nextLine();
+                resumeArrayInfo[bug] = in.nextLine();
                 System.out.println("Выберите критичность дефекта: \n 1. Блокирующий \n 2. Высокий \n 3. Средний \n 4. Низкий");
                 //ввод критичности из консоли
                 int choice = in.nextInt();
                 //переменная для критичности
-                String criticality;
                 //отображать сообщение только если введено некорректное значение критичности
                 if (choice > CRITICALITY_ARRAY.length || choice <= 0){
                     do {
                         System.out.println("Необходимо выбрать порядковый номер критичности от 1 до " + CRITICALITY_ARRAY.length);
                         choice = in.nextInt();
                     } while (choice > CRITICALITY_ARRAY.length || choice <= 0);
-                    criticality = CRITICALITY_ARRAY[choice - 1];
+                    criticalityArrayInfo[bug] = CRITICALITY_ARRAY[choice - 1];
                 } else {
-                    criticality = CRITICALITY_ARRAY[choice - 1];
+                    criticalityArrayInfo[bug] = CRITICALITY_ARRAY[choice - 1];
                 }
 
                 System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
                 //переменная для срока исправления
-                int date = in.nextInt();
-                String info = "[" + (bug) + "] " + "Резюме: " + resume + " | Критичность: " + criticality + " | Дней на исправление: " + date
-                        + " | Исправление займёт больше рабочей недели: " + (date > WORK_WEEK);
+                daysToFixArrayInfo[bug] = in.nextInt();
+                String info = "[" + (bug) + "] " + "Резюме: " + resumeArrayInfo[bug] +
+                        " | Критичность: " + criticalityArrayInfo[bug] +
+                        " | Дней на исправление: " + daysToFixArrayInfo[bug] +
+                        " | Исправление займёт больше рабочей недели: " + (daysToFixArrayInfo[bug] > WORK_WEEK);
                 arrayOfBugs[bug] = info;
                 bug++;
             } else if (action == 2) {
