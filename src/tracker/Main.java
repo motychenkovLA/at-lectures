c12aa09e2edb84b74191e932f7e7dc46c0dd36db
siroezhka27 <siroezhka13@gmail.com>
2021-02-16 15:02:35 +0300

    Задание "3"

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -5,25 +5,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final int CONST = 5;
-        boolean t;
+        final int week = 5;
+        int bugs = 10;
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        String[] summary = new String[bugs];
+        String[] criticality = new String[bugs];
+        int days[] = new int[bugs];
 
-        Scanner ra = new Scanner(System.in);
-        System.out.println("Введите резюме дефекта:");
-        String r = ra.nextLine();
+        do {
+            System.out.println("Для добавления нового дефекта ввведите \"add\" \nДля просмотра списка дефектов введите \"list\" \nДля выхода из программы введите \"quit\" ");
+            String menu = sc.nextLine();
+            if (menu.equals("add")) {
+                if (count < bugs) {
+                    System.out.println("Введите резюме дефекта:");
+                    summary[count] = sc.nextLine();
 
-        Scanner ca = new Scanner(System.in);
-        System.out.println("Введите критичность дефекта (блокирующая, критическая, средняя, низкая):");
-        String c = ca.nextLine();
+                    System.out.println("Введите критичность дефекта (блокирующая, критическая, средняя, низкая):");
+                    criticality[count] = sc.nextLine();
 
-        Scanner da = new Scanner(System.in);
-        System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
-        int d = da.nextInt();
-        t = (d > CONST);
-
-            System.out.println("резюме " +r+ " | серьезность " +c+ " | количество дней на исправление " +d+ " | займет больше рабочей недели " +t);
-
-
-    }
-
+                    System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
+                    days[count] = sc.nextInt();
+                    sc.nextLine();
+                    count++;
+                } else System.out.println("Нет возможности ввода более 10 дефектов");
+           }
+            else if (menu.equals("list")) {
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("номер дефекта:  " + (i+1) + " | резюме: " + summary[0] + " | серьезность: " + criticality[0] + " | количество дней на исправление " + days[0]);
+                    }
+                } else if (menu.equals("quit")) {
+                    break;
+                }
+            }
+            while (count != bugs) ;
+              }
 }
