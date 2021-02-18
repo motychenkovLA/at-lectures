e1c07f3b4d4f79e09f2091520ead39eba894c385
gkiselev <gkiselev@tsconsulting.com>
2021-02-18 21:33:17 +0300

    домашняя работа 3.2 Добавлен класс

--- src/tracker/Defect.java
+++ src/tracker/Defect.java
@@ -6,19 +6,51 @@ public class Defect {
     при создании экземпляра дефекта поле id заполнять порядковым номером созданного дефекта
     при выводе списка дефекты отображаются в виде "id | резюме | серьезность | количество дней на исправление"
     */
+    public final static int WORK_WEEK = 5;
+    private long id = 0;
+    private String resume;
+    private String severity;
+    private int dueDates;
 
-   private long id = 0;
-   private String resume;
-   private String criticality;
-   private int correctionDate;
-
-   public Defect (String resume, String criticality, int correctionDate){
+   public Defect (String resume, String severity, int dueDates){
        this.id++;
        this.resume = resume;
-       this.criticality = criticality;
+       this.severity = severity;
+       this.dueDates = dueDates;
+   }
+
+   /* public String setResume (String resume){
+       return this.resume = resume;
+   }
+
+    public void setCriticality (String criticality){
+     this.severity = criticality;
+   }
+
+    public void setCorrectionDate (int correctionDate){
        this.correctionDate = correctionDate;
    }
 
+   public String getResume (){
+       return resume;
+   }
 
+    public String getCriticality (){
+        return severity;
+    }
+
+    public int  getCorrectionDate(){
+        return correctionDate;
+    }
+
+    public long  getId(){
+       return id;
+    }*/
+
+   public void getInfo(){
+        System.out.println(id + " | Резюме: " + resume + " | Критичность дефекта: " + severity +
+                " | Количество дней на исправление: " + dueDates +
+                " | Исправление дефекта займет больше рабочей недели: " + (dueDates > WORK_WEEK));
+    }
 
 }
--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -4,55 +4,50 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //выбор клиента
+
         int userChoice;
         //счетчик кол-ва дефектов
         int countBugs = 0;
-        //константа кол-ва дней в рабочей неделе
-        final int WORK_WEEK = 5;
-        //константа кол-ва дефектов
         final int MAX_COUNT_BUGS = 10;
         //создаем объект класса сканнер
         Scanner sc = new Scanner(System.in);
-
-        //массив описания дефектов
-        String arrayOfResume[] = new String[MAX_COUNT_BUGS];
-        String arrayOfCriticality[] = new String[MAX_COUNT_BUGS];
-        int arrayOfCorrectionDate[] = new int[MAX_COUNT_BUGS];
+        Defect[] bugs = new Defect[MAX_COUNT_BUGS];
 
         do {
             System.out.print("Доступные действия:\n1. Добавить дефект\n2. Вывести список дефектов" +
                     "\n3. Выйти из программы\nВыберите действие: ");
             userChoice = sc.nextInt();
-            //добавляем nextLine() после nextInt, nextShort и т.д., для последующего использования nextLine()
+            //добавляем nextLine() после nextInt, nextShort и т.д.,
+            // для последующего использования nextLine()
             sc.nextLine();
             if (userChoice == 1) {
                 if(countBugs < MAX_COUNT_BUGS) {
-                    System.out.println("----------\nУкажите резюме дефекта: ");
-                    //ввод с клавиатуры описания дефекта
-                    arrayOfResume[countBugs]= sc.nextLine();
-                    System.out.print("----------\nВыберите приоритет дефекта:\n1. Блокирующий\n2. Высокий\n" +
-                            "3. Средний\n4. Низкий\nУкажите приоритет дефекта: ");
+                    System.out.print("\nУкажите резюме дефекта: ");
+                    String resume = sc.nextLine();
+                    System.out.print("\nВыберите приоритет дефекта:\n1. Блокирующий\n2. Высокий\n3. Средний" +
+                            "\n4. Низкий\nУкажите приоритет дефекта: ");
                     //ввод с клаиватуры критичности дефекта
-                    arrayOfCriticality[countBugs] = sc.nextLine();
-                  System.out.println("----------\nУкажите ожидаемое количество дней на исправление дефекта");
-                    //ввод кол-ва дней исправления дефекта
-                    arrayOfCorrectionDate[countBugs]= sc.nextInt();
+                    String severity = sc.nextLine();
+                  System.out.println("\nУкажите ожидаемое количество дней на исправление дефекта");
+                    int dueDates = sc.nextInt();
                     sc.nextLine();
-                    //увеличиваем счетчик дефектов на 1
+                    bugs [countBugs] = new Defect(resume, severity, dueDates);
                     countBugs++;
-                } else System.out.println("----------\nНевозможно создать более 10 дефектов\n----------");
+
+                } else {
+                    System.out.println("\nНевозможно создать более 10 дефектов\n");
+                }
             } else if (userChoice == 2) {
                 if (countBugs > 0) {
                     //System.out.println(countBugs);
-                    for (int i = 0; i < countBugs; i++)
-                    System.out.println("Дефект № " + (i+1) + " | Резюме: " + arrayOfResume[i] + " | Критичность дефекта: " +
-                            arrayOfCriticality[i] + " | Количество дней на исправление: " + arrayOfCorrectionDate[i] +
-                            " | Исправление дефекта займет больше рабочей недели: " + (arrayOfCorrectionDate[i] > WORK_WEEK));
-                } else System.out.println("----------\nСписок дефектов пуст\n----------");
+                    for (int i = 0; i < countBugs; i++) {
+                        bugs[i].getInfo();
+                    }
+                } else {
+                    System.out.println("\nСписок дефектов пуст\n");
+                }
             }
-        }
-            while (userChoice != 3);
+        } while (userChoice != 3);
     }
 }
 
